c787142b6e7572ea934b376a1356fa4def19c29a
Valmir Cardoso Ribeiro <valmir@hospitalsaojose.com.br>
2023-11-09 14:37:22 -0300

    Backend Users finalizado

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,7 +3,7 @@ const {User: UserModel} = require("../models/User")
 const userController ={
     create: async(req, res) => {
         try{
-            const users ={
+            const user ={
                 nm_usuario: req.body.nm_usuario,
                 login: req.body.login,
                 senha: req.body.senha,
@@ -13,7 +13,7 @@ const userController ={
                 dt_nasc: req.body.dt_nasc,
                 dt_val_licenca: req.body.dt_val_licenca,
             }
-            const response = await UserModel.create(users);
+            const response = await UserModel.create(user);
 
             res.status(201).json({response, msg: "Usuário criado com sucesso!"})
 
@@ -27,6 +27,66 @@ const userController ={
             const users = await UserModel.find()
             res.json(users)
         } catch(error){
+            console.log(error)
+        }
+    },
+
+    get: async(req, res) =>{
+        try{
+            const id = req.params.id
+            const user = await UserModel.findById(id)
+            if(!user){
+                res.status(404).json({msg: "Usuário não encontrado."})
+                return
+            }
+
+            res.json(user)
+        }catch(error){
+            console.log(error)
+        }
+    },
+
+    delete: async(req, res) =>{
+        try{
+            const id = req.params.id
+            const user = await UserModel.findById(id)
+            if(!user){
+                res.status(404).json({msg: "Usuário não encontrado."})
+                return
+            }
+
+            const deletedUser = await UserModel.findByIdAndDelete(id)
+            res.status(200).json({deletedUser, msg: "Usuário deletado com sucesso!"})
+        }catch(error){
+            console.log(error)
+        }
+    },
+
+    update: async(req,res) => {
+        try{
+            const id = req.params.id
+
+            const user ={
+                nm_usuario: req.body.nm_usuario,
+                login: req.body.login,
+                senha: req.body.senha,
+                cpf: req.body.cpf,
+                email: req.body.email,
+                telefone: req.body.telefone,
+                dt_nasc: req.body.dt_nasc,
+                dt_val_licenca: req.body.dt_val_licenca,
+            }
+
+            const updatedUser = await UserModel.findByIdAndUpdate(id, user)
+
+            if(!updatedUser){
+                res.status(404).json({msg: "Usuário não encontrado."})
+                return
+            }
+            res.status(200).json({user, msg: "Usuário atualizado com sucesso!"})
+
+
+        }catch(error){
             console.log(error)
         }
     }
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -10,4 +10,16 @@ router
     .route("/users")
     .get((req, res) => userController.getAll(req, res))
 
+router
+    .route("/users/:id")
+    .get((req,res) => userController.get(req,res))
+
+router
+    .route("/users/:id")
+    .delete((req,res) => userController.delete(req,res))
+
+router
+    .route("/users/:id")
+    .put((req,res) => userController.update(req,res))
+
 module.exports = router
