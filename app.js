9a602b467697bad29f8ac9aaa8658f4b163f65c1
DSM-3 <dsm3@fatec.academico>
2023-11-16 22:03:59 -0300

    Tela de cadastro

--- app.js
+++ app.js
@@ -20,18 +20,21 @@ app.set('view engine', 'ejs');
 
 app.use(express.static('./public'));
 
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
     res.render("index")
 })
 
-app.get("/home", function (req, res) {
+app.get("/home", (req, res) => {
     res.render("home")
 })
 
-app.get("/analise", function (req, res) {
+app.get("/analise", (req, res) => {
     res.render("analise")
 })
 
-app.listen(3000, function () {
+app.get("/signup", (req, res) => {
+    res.render("signup")
+})
+app.listen(3000, () => {
     console.log("Servidor iniciado com sucesso!")
 })
